027003b3686b151960726b7de8a965240ea55da4
Shivanithalla <thallashivani1502@gmail.com>
2025-08-12 13:07:00 +0530

    Update 01-arrow-functions.js

--- arrowFunction-asyncAwait-regex/01-arrow-functions.js
+++ arrowFunction-asyncAwait-regex/01-arrow-functions.js
@@ -9,60 +9,62 @@ function add(a, b) {
     return a + b;
 }
 // const add = // YOUR CODE HERE
+const add = (a, b) => a + b;
 
 // TODO: Convert this function expression to an arrow function
 const multiply = function(x, y) {
     return x * y;
 };
 // const multiply = // YOUR CODE HERE
+const multiply = (x, y) => x * y;
 
 // 2. Single Parameter Arrow Functions
 // TODO: Create an arrow function that doubles a number (no parentheses needed)
-// const double = // YOUR CODE HERE
+const double = n => n * 2;
 
 // TODO: Create an arrow function that returns the length of a string
-// const getLength = // YOUR CODE HERE
+const getLength = str => str.length;
 
 // 3. Implicit Return
 // TODO: Convert to arrow function with implicit return
 function square(n) {
     return n * n;
 }
-// const square = // YOUR CODE HERE
+const square = n => n * n;
 
 // TODO: Create arrow function that returns an object (hint: wrap in parentheses)
 function createUser(name, age) {
     return { name: name, age: age };
 }
-// const createUser = // YOUR CODE HERE
+const createUser = (name, age) => ({ name, age });
 
 // 4. Array Methods with Arrow Functions
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // TODO: Use arrow function with map to square all numbers
-// const squared = numbers.map(/* YOUR ARROW FUNCTION HERE */);
+const squared = numbers.map(num => num * num);
 
 // TODO: Use arrow function with filter to get even numbers
-// const evens = numbers.filter(/* YOUR ARROW FUNCTION HERE */);
+const evens = numbers.filter(num => num % 2 === 0);
 
 // TODO: Use arrow function with reduce to sum all numbers
-// const sum = numbers.reduce(/* YOUR ARROW FUNCTION HERE */);
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
 
 // 5. Arrow Functions vs Regular Functions (this binding)
 const obj = {
     name: 'Training Object',
     
-    // TODO: Fix this method to properly access this.name using arrow function
     regularMethod: function() {
         setTimeout(function() {
-            console.log('Regular function:', this.name); // undefined
+         console.log('Regular function:', this.name); // undefined
         }, 100);
     },
     
-    // TODO: Create an arrow method that correctly accesses this.name
-    // arrowMethod: function() {
-    //     setTimeout(/* YOUR ARROW FUNCTION HERE */, 100);
-    // }
+    arrowMethod: function() {
+        setTimeout(() => {
+            console.log('Arrow function:', this.name); // 'Training Object'
+        }, 100);
+    }
 };
 
 // 6. Complex Arrow Function Scenarios
@@ -73,16 +75,16 @@ const users = [
 ];
 
 // TODO: Chain methods - get names of active users, uppercase them
-// const activeUserNames = users
-//     .filter(/* YOUR ARROW FUNCTION HERE */)
-//     .map(/* YOUR ARROW FUNCTION HERE */);
+const activeUserNames = users
+    .filter(user => user.active)
+    .map(user => user.name.toUpperCase());
 
 // TODO: Find user with specific criteria using arrow function
-// const userOver30 = users.find(/* YOUR ARROW FUNCTION HERE */);
+const userOver30 = users.find(user => user.age > 30);
 
 // 7. Advanced: Higher-Order Functions
 // TODO: Create a higher-order function that returns an arrow function
-// const multiplyBy = (factor) => /* YOUR ARROW FUNCTION HERE */;
+const multiplyBy = factor => number => factor * number;
 
 // Test your functions (uncomment after completing TODOs):
 /*
